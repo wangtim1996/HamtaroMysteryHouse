b19b481b24b12f8bf9b4799d034c8f6e3e92e524
WEDSGARBAGEDAY <glynn96@gmail.com>
2019-01-27 18:22:13 -0800

    main menu changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    private AsyncOperation loadGame;
+    public GameObject loadingBar;
+    public GameObject[] deactivateDuringLoad;
+
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 );
+        loadGame = SceneManager.LoadSceneAsync("main");
+        loadingBar.SetActive(true);
+        foreach(var o in deactivateDuringLoad)
+        {
+            o.SetActive(false);
+        }
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    void Update()
+    {
+        if(loadGame != null)
+        {
+            var progress = loadGame.progress;
+            loadingBar.GetComponent<Slider>().value = progress;
+        }
     }
 }
